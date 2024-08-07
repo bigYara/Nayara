1b715f0bc231b47f7f3564bd718190d24606ed41
Nayara <161309775+bigYara@users.noreply.github.com>
2024-08-07 11:59:36 -0300

    Add files via upload

---
+++ tcc/index.php
@@ -0,0 +1,192 @@
+<php?
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tcc</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link href="style.css" rel="stylesheet">
+</head>
+<body>
+  
+<nav class="navbar navbar-expand-lg cor">
+  <div class="container-fluid">
+    <img src="logo.png">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="menu">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav text-white">
+        <li class="nav-item">
+          <a class="nav-link text-white active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Mais Vendidas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled text-white" aria-disabled="true">Informações</a>
+        </li>
+      </ul>
+      <br>
+      
+      </form>
+    </div>
+  </div>
+</div>
+</nav>
+<br>
+<!-- cards das plantas 1-->
+<div class="container">
+  <div class="row">
+    
+  <div class="col-3 mt-4">
+<div class="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSS7HlYOkAUYPjk5YyNyfsjGJIt089MIeq9g&s" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Peperomia scandens</h5>
+    <p class="card-text"> peperômia atrai energias criativas e incentiva o crescimento pessoal.</p>
+    <div class="form-check form-switch">
+  <input class="form-check-input input_escolha" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+  <label class="form-check-label " for="flexSwitchCheckDefault">Selecionar</label>
+</div>
+</div>
+  </div>
+</div>
+
+<!-- cards das plantas 2-->
+<div class="col-3 mt-4">
+<div class="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSS7HlYOkAUYPjk5YyNyfsjGJIt089MIeq9g&s" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Peperomia scandens</h5>
+    <p class="card-text"> peperômia atrai energias criativas e incentiva o crescimento pessoal.</p>
+    <div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+  <label class="form-check-label" for="flexSwitchCheckDefault">Selecionar</label>
+</div>
+</div>
+  </div>
+</div>
+
+<!-- cards das plantas 3-->
+<div class="col-3 mt-4">
+<div class="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSS7HlYOkAUYPjk5YyNyfsjGJIt089MIeq9g&s" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Peperomia scandens</h5>
+    <p class="card-text"> peperômia atrai energias criativas e incentiva o crescimento pessoal.</p>
+    <div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+  <label class="form-check-label" for="flexSwitchCheckDefault">Selecionar</label>
+</div>
+  </div>
+</div>
+</div>
+
+<!-- cards das plantas 4-->
+<div class="col-3 mt-4">
+<div class="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSS7HlYOkAUYPjk5YyNyfsjGJIt089MIeq9g&s" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Peperomia scandens</h5>
+    <p class="card-text"> peperômia atrai energias criativas e incentiva o crescimento pessoal.</p>
+    <div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+  <label class="form-check-label" for="flexSwitchCheckDefault">Selecionar</label>
+</div>
+  </div>
+</div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<!-- cards das plantas 5-->
+
+  <div class="col-3 mt-4">
+<div class="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSS7HlYOkAUYPjk5YyNyfsjGJIt089MIeq9g&s" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Peperomia scandens</h5>
+    <p class="card-text"> peperômia atrai energias criativas e incentiva o crescimento pessoal.</p>
+    <div class="form-check form-switch">
+  <input class="form-check-input input_escolha" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+  <label class="form-check-label " for="flexSwitchCheckDefault">Selecionar</label>
+</div>
+</div>
+  </div>
+</div>
+
+<!-- cards das plantas 6-->
+<div class="col-3 mt-4">
+<div class="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSS7HlYOkAUYPjk5YyNyfsjGJIt089MIeq9g&s" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Peperomia scandens</h5>
+    <p class="card-text"> peperômia atrai energias criativas e incentiva o crescimento pessoal.</p>
+    <div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+  <label class="form-check-label" for="flexSwitchCheckDefault">Selecionar</label>
+</div>
+</div>
+  </div>
+</div>
+
+<!-- cards das plantas 7-->
+<div class="col-3 mt-4">
+<div class="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSS7HlYOkAUYPjk5YyNyfsjGJIt089MIeq9g&s" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Peperomia scandens</h5>
+    <p class="card-text"> peperômia atrai energias criativas e incentiva o crescimento pessoal.</p>
+    <div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+  <label class="form-check-label" for="flexSwitchCheckDefault">Selecionar</label>
+</div>
+  </div>
+</div>
+</div>
+
+<!-- cards das plantas 8-->
+<div class="col-3 mt-4">
+<div class="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSS7HlYOkAUYPjk5YyNyfsjGJIt089MIeq9g&s" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Peperomia scandens</h5>
+    <p class="card-text"> peperômia atrai energias criativas e incentiva o crescimento pessoal.</p>
+    <div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+  <label class="form-check-label" for="flexSwitchCheckDefault">Selecionar</label>
+</div>
+  </div>
+</div>
+</div>
+
+
+
+
+
+
+</div>
+</div>
+</div>
+
+          
+    
+ 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+
+
+
+</body>
+</html>
